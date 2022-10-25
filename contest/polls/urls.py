7d6238c23e64981cba65141c3c68aa92d9511abb
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2022-10-25 20:37:31 +0800

    第 4 部分
    https://docs.djangoproject.com/zh-hans/4.1/intro/tutorial04/
    
    Co-authored-by: Phoupraw <Phoupraw@users.noreply.github.com>

--- contest/polls/urls.py
+++ contest/polls/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = "polls"
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:question_id>/", views.detail, name="detail"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- contest/polls/views.py
+++ contest/polls/views.py
@@ -1,21 +1,42 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
-from .models import Question
-
-
-def index(request: HttpRequest) -> HttpResponse:
-    return render(
-        request,
-        "polls/index.html",
-        {"question_list": Question.objects.all()},
-    )
+from .models import Question, Choice
 
 
-def detail(request: HttpRequest, question_id: int) -> HttpResponse:
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+
+    def get_queryset(self):
+        return Question.objects.all()
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name: str = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name: str = "polls/results.html"
+
+
+def vote(request: HttpRequest, question_id: int) -> HttpResponse:
     question = get_object_or_404(Question, pk=question_id)
-    return render(
-        request,
-        "polls/detail.html",
-        {"question": question},
-    )
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "你尚未选择。",
+            },
+        )
+    else:
+        # 这里不会真的投票。
+        print(f"有人投了票：{selected_choice.content}。")
+        return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
