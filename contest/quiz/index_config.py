b568ae96176b0f93b09ec21090c7ba00b6c9a4d4
XDT6 <taossd@qq.com>
2023-07-13 23:53:29 +0800

    完成了首页的前端样式

---
+++ contest/quiz/index_config.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+def index_config(request):
+    config = {
+        'year':'2023',      # 年
+        'month':'9',        # 月
+        'cnt':'10',         # 题目数
+        'time_limit':'150', # 限时（秒）
+        'change':'2',       # 答题次数
+    }
+
+    return render(request, 'index.html', {"config":config})
