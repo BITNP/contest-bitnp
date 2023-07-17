7e81158755d660e23906bcd47e49194ddb7e1049
XDT6 <taossd@qq.com>
2023-07-17 13:37:37 +0800

    将 Navigation 与 footer 拆开放入 base.html 中

--- contest/quiz/urls.py
+++ contest/quiz/urls.py
@@ -1,13 +1,13 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from . import views, index_config
+from . import config, views
 
 app_name = "quiz"
 
 urlpatterns = [
     # path("", views.IndexView.as_view(), name="index"),
-    path("index/", index_config.index_config, name="index"),
+    path("index/", config.index_config, name="index"),
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
