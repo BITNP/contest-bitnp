893cd5befe0d345983eea437d4258ded08a6c357
XDT6 <taossd@qq.com>
2023-07-17 19:52:20 +0800

    个人中心前端样式（部分完成）

--- contest/quiz/urls.py
+++ contest/quiz/urls.py
@@ -8,6 +8,7 @@ app_name = "quiz"
 urlpatterns = [
     # path("", views.IndexView.as_view(), name="index"),
     path("index/", config.index_config, name="index"),
+    path("info/", config.info_config, name="info"),
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
