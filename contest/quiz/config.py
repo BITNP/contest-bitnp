c1ed955ae2bb45e0d8e0c0f24afcc239e6510d82
XDT6 <taossd@qq.com>
2023-07-17 19:52:20 +0800

    个人中心前端样式（部分完成）

--- contest/quiz/config.py
+++ contest/quiz/config.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render
 
+config_origin = {
+    'year':'2023',      # 年
+    'month':'9',        # 月
+
+    # 试题相关
+    'score':'100',      # 总分
+    'cnt':'10',         # 题目数
+    'time_limit':'150', # 限时（秒）
+    'change':'2',       # 答题次数
+}
+
 def index_config(request):
     config = {
-        'year':'2023',      # 年
-        'month':'9',        # 月
-        'cnt':'10',         # 题目数
-        'time_limit':'150', # 限时（秒）
-        'change':'2',       # 答题次数
+        'year':config_origin['year'],
+        'month':config_origin['month'],
+        'cnt':config_origin['cnt'],
+        'time_limit':config_origin['time_limit'],
+        'change':config_origin['change'],
     }
-    return render(request, 'index.html', {"config":config})
+    return render(request, 'index.html', {'config':config})
 
-def base_config(request):
+def info_config(request):
     config = {
-        'year':'2023',
+        'year':config_origin['year'],
+        'month':config_origin['month'],
+        'score':config_origin['score'],
+        'change':config_origin['change'],
     }
-    return render(request, 'base.html', {'config':config})
+    return render(request, 'info.html', {'config':config})
