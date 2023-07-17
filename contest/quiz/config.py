f8a205b717cf3cafdf7250abe05cf81894b67278
XDT6 <taossd@qq.com>
2023-07-17 13:37:37 +0800

    将 Navigation 与 footer 拆开放入 base.html 中

--- contest/quiz/config.py
+++ contest/quiz/config.py
@@ -8,5 +8,10 @@ def index_config(request):
         'time_limit':'150', # 限时（秒）
         'change':'2',       # 答题次数
     }
-
     return render(request, 'index.html', {"config":config})
+
+def base_config(request):
+    config = {
+        'year':'2023',
+    }
+    return render(request, 'base.html', {'config':config})
