ad5c941c64f9b055b4de35e15b328941f1c91918
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2023-07-31 18:23:36 +0800

    feat: User name in templates

--- contest/quiz/constants.py
+++ contest/quiz/constants.py
@@ -20,11 +20,9 @@ class ConstantsNamespace:
     """一套题的总分"""
 
     ROUTES = {
-        "quiz:index": {"title": "主页", "scenario": "both"},
-        "quiz:contest": {"title": "答题", "scenario": "logged_in"},
-        "quiz:info": {"title": "个人中心", "scenario": "logged_in"},
-        "logout": {"title": "登出", "scenario": "logged_in"},
-        "login": {"title": "登录", "scenario": "logged_out"},
+        "quiz:index": {"title": "主页", "login_required": False},
+        "quiz:contest": {"title": "答题", "login_required": True},
+        "quiz:info": {"title": "个人中心", "login_required": True},
     }
 
 
