742aa7d2a83fe9e441dbf15b3275aeae0def5410
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2023-08-02 18:51:16 +0800

    build: Webpack → rollup

---
+++ contest/js/static_src/rollup.config.prod.mjs
@@ -0,0 +1,20 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import { babel } from '@rollup/plugin-babel'
+import terser from '@rollup/plugin-terser'
+
+import dev_config from './rollup.config.dev.mjs'
+
+export default {
+  input: dev_config.input,
+  output: {
+    ...dev_config.output,
+    compact: true,
+  },
+  plugins: [
+    commonjs(),
+    resolve(),
+    babel({ babelHelpers: 'bundled' }),
+    terser(),
+  ],
+}
