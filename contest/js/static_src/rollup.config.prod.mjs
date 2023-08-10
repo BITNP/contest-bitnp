744c4e9668f54b7807ba78a29d82c2edfe27a162
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2023-08-10 13:11:34 +0800

    lint: ESLint

--- contest/js/static_src/rollup.config.prod.mjs
+++ contest/js/static_src/rollup.config.prod.mjs
@@ -6,15 +6,15 @@ import terser from '@rollup/plugin-terser'
 import dev_config from './rollup.config.dev.mjs'
 
 export default {
-  input: dev_config.input,
-  output: {
-    ...dev_config.output,
-    compact: true,
-  },
-  plugins: [
-    commonjs(),
-    resolve(),
-    babel({ babelHelpers: 'bundled' }),
-    terser(),
-  ],
+    input: dev_config.input,
+    output: {
+        ...dev_config.output,
+        compact: true,
+    },
+    plugins: [
+        commonjs(),
+        resolve(),
+        babel({ babelHelpers: 'bundled' }),
+        terser(),
+    ],
 }
--- contest/js/static_src/src/status.js
+++ contest/js/static_src/src/status.js
@@ -5,7 +5,7 @@ import { get_data } from './util'
 /** @type {"not taking" | "taking contest" | "deadline passed" | ""} */
 const status = get_data('status')
 
-if (status == 'deadline passed') {
+if (status === 'deadline passed') {
     Swal.fire({
         title: '上次作答已超时',
         text: '截止前作答部分已尽量保存。建议下次早些提交。',
