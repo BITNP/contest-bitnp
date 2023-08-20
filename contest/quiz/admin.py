0c53958253306bd284092f1bafa9e2b1b1b90964
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2023-08-20 16:47:38 +0800

    feat(admin): Score filters
    Slow, but better than nothing.

--- contest/quiz/admin.py
+++ contest/quiz/admin.py
@@ -1,3 +1,8 @@
+from __future__ import annotations
+
+from operator import methodcaller
+from typing import TYPE_CHECKING
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
@@ -11,6 +16,10 @@ from .models import (
     Student,
     User,
 )
+
+if TYPE_CHECKING:
+    from django.db.models import QuerySet
+    from django.http import HttpRequest
 
 admin.site.register(User, UserAdmin)
 
@@ -32,12 +41,53 @@ class AnswerInline(admin.TabularInline):
     extra = 0
 
 
+class ScoreFilter(admin.SimpleListFilter):
+    title = "得分"
+    parameter_name = "score_interval"
+    key = methodcaller("score")
+
+    def lookups(
+        self, request: HttpRequest, model_admin: admin.ModelAdmin
+    ) -> list[tuple[str, str]]:
+        """Get a list of URL queries and human-readable names"""
+        breakpoints = [0, 60, 70, 80, 90, 100]
+        return [
+            (f"{low}–{high}", f"[{low}, {high})")
+            for low, high in zip(breakpoints[:-1], breakpoints[1:])
+        ] + [
+            (f"{breakpoints[-1]}–", f"[{breakpoints[-1]}, +∞)"),
+        ]
+
+    def queryset(self, request: HttpRequest, queryset: QuerySet) -> QuerySet | None:
+        """Filter the query
+
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        if (value := self.value()) is None:
+            return None
+
+        interval = value.split("–")
+        if len(interval) != 2:
+            return None
+
+        if interval[1] != "":
+            low, high = map(int, interval)
+            hits = [i.pk for i in queryset if low <= self.key(i) < high]
+            # Slow, but better than nothing
+        else:
+            hits = [i.pk for i in queryset if int(interval[0]) <= self.key(i)]
+
+        return queryset.filter(pk__in=hits)
+
+
 @admin.register(Response)
 class ResponseAdmin(admin.ModelAdmin):
     fields = ["student", "submit_at", "score"]
     readonly_fields = ["score"]
     inlines = [AnswerInline]
-    list_filter = ["submit_at"]
+    list_filter = ["submit_at", ScoreFilter]
     list_display = ["student", "submit_at", "score"]
 
 
@@ -66,12 +116,18 @@ class DraftResponseInline(admin.StackedInline):
     extra = 0
 
 
+class FinalScoreFilter(ScoreFilter):
+    title = "最终得分"
+    parameter_name = "final_score_interval"
+    key = methodcaller("final_score")
+
+
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     fields = ["user", "name", "final_score"]
     readonly_fields = ["final_score"]
     search_fields = ["name", "user__username"]
-    # todo: list_filter = ["final_score"]
+    list_filter = [FinalScoreFilter]
     list_display = ["name", "final_score"]
 
     inlines = [ResponseInline, DraftResponseInline]
