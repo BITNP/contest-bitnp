83851b8b687a6d6dfa956904b913c03a48028dbb
everything411 <everything411@163.com>
2023-08-26 14:16:53 +0800

    deploy: use separate db path

--- contest/contest/settings.py
+++ contest/contest/settings.py
@@ -136,6 +136,14 @@ DATABASES = {
     }
 }
 
+if getenv("DJANGO_PRODUCTION"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db" / "db.sqlite3",
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
