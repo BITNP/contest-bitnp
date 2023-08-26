6d3265c68a83f183a76232b23923017d3db61cf3
everything411 <everything411@163.com>
2023-08-26 14:14:38 +0800

    deploy: small fixes

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-/usr/sbin/nginx &
+/usr/sbin/nginx
 python3 manage.py migrate
 gunicorn -w 4 --forwarded-allow-ips='*' -b unix:/tmp/gunicorn.sock -k uvicorn.workers.UvicornWorker contest.asgi:application
